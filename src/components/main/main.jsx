ec10991bdd10dd48da7f0ee1a28fd847ed80e83a
Stephen Han <shan@convoy.com>
2020-06-13 17:44:19 -0700

    Add pickup location distance calculation

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,33 +1,81 @@
-import React from 'react'
-import SearchBox from '../searchbox/searchbox'
-import Results from '../results/results'
-import PICKUP_LOCATIONS from '../../constants/ysPickupLocations'
+import React from "react";
+import SearchBox from "../searchbox/searchbox";
+import Results from "../results/results";
+import PICKUP_LOCATIONS from "../../constants/ysPickupLocations";
 
-export class Main extends React.Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            results: null
-        };
+export class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      results: null,
+    };
 
-        this.searchForNearestPickup = function (location) {
-            console.log(location)
-            // To Do: Make Google Maps call to get results and replace PICKUP_LOCATIONS below
+    this.searchForNearestPickup = function (location) {
+      console.log(
+        "Calling searchForNeearestPickup: " + JSON.stringify(location)
+      );
+      // To Do: Make Google Maps call to get results and replace PICKUP_LOCATIONS below
 
-            this.setState({
-                results: PICKUP_LOCATIONS
-            })
-        }
-    }
+      var origin = new google.maps.LatLng(
+        location[0].geometry.location.lat(),
+        location[0].geometry.location.lng()
+      );
 
-    render() {
-        return (
-            <div className="main__container">
-                <SearchBox searchForNearestPickup={(i) => this.searchForNearestPickup(i)}/>
-                <Results results={this.state.results}/>
-            </div>
-        )
-    }
+      var service = new google.maps.DistanceMatrixService();
+
+      var pickup_by_distance = [];
+
+      this.setState({
+        results: pickup_by_distance,
+      }, () => {
+        PICKUP_LOCATIONS.forEach(function (pickup) {
+            console.log("pickup: " + JSON.stringify(pickup));
+    
+            var destination = new google.maps.LatLng(
+              pickup.latitude,
+              pickup.longitude
+            );
+            service.getDistanceMatrix(
+              {
+                origins: [origin],
+                destinations: [destination],
+                travelMode: "DRIVING",
+                avoidHighways: false,
+                avoidTolls: false,
+              },
+              function callback(response, status) {
+                console.log("Distance: " + JSON.stringify(response));
+                var results = response.rows[0].elements;
+    
+                pickup_by_distance.push({
+                  ...pickup,
+                  results,
+                });
+    
+                console.log("Final Array: " + JSON.stringify(pickup_by_distance));
+              }
+            );
+          });
+      });
+
+    //   function callback(response, status) {
+    //     console.log("Distance: " + JSON.stringify(response));
+    //     // See Parsing the Results for
+    //     // the basics of a callback function.
+    //   }
+    };
+  }
+
+  render() {
+    return (
+      <div className="main__container">
+        <SearchBox
+          searchForNearestPickup={(i) => this.searchForNearestPickup(i)}
+        />
+        <Results results={this.state.results} />
+      </div>
+    );
+  }
 }
 
-export default Main
+export default Main;
--- src/constants/ysPickupLocations.js
+++ src/constants/ysPickupLocations.js
@@ -7,8 +7,8 @@ const PICKUP_LOCATIONS = [
     state_code: "IA",
     zip: "50401",
     dates_hours: "1:00 PM - 3:00 PM",
-    latitude: null,
-    longitude: null,
+    latitude: 43.148570,
+    longitude: -93.141733,
     signs: 100
   },
   {
@@ -19,8 +19,8 @@ const PICKUP_LOCATIONS = [
     state_code: "IA",
     zip: "51639",
     dates_hours: "3:00 PM - 5:00 PM",
-    latitude: null,
-    longitude: null,
+    latitude: 40.728748,
+    longitude: -95.460029,
     signs: 100
   },
   {
@@ -31,8 +31,8 @@ const PICKUP_LOCATIONS = [
     state_code: "IA",
     zip: "52722",
     dates_hours: "1:00 PM - 3:00 PM",
-    latitude: null,
-    longitude: null,
+    latitude: 41.562796,
+    longitude: -90.487070,
     signs: 100
   },
   {
@@ -43,8 +43,8 @@ const PICKUP_LOCATIONS = [
     state_code: "IA",
     zip: "51333",
     dates_hours: "1:00 PM - 3:00 PM",
-    latitude: null,
-    longitude: null,
+    latitude: 43.097185,
+    longitude: -95.030741,
     signs: 100
   },
 ];
