9aed96c7d6b1e5bdf69d19357d4d62d443362a7d
Stephen Han <shan@convoy.com>
2020-07-22 01:56:24 -0700

    Add distance calculation

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -11,12 +11,83 @@ export class Main extends React.Component{
             results: null
         };
 
-        this.searchForNearestPickup = function(location) {
+
+        this.searchForNearestPickup = async function (location) {
+            console.log(
+              "Calling searchForNearestPickup: " + JSON.stringify(location)
+            );
             // To Do: Make Google Maps call to get results and replace PICKUP_LOCATIONS below
+      
+            var origin = new google.maps.LatLng(
+              location[0].geometry.location.lat(),
+              location[0].geometry.location.lng()
+            );
+      
+            var service = new google.maps.DistanceMatrixService();
+      
+            var pickup_by_distance = [];
+      
+            for (let i = 0; i < PICKUP_LOCATIONS.length; i++) {
+              const pickup = PICKUP_LOCATIONS[i];
+              console.log("pickup: " + JSON.stringify(pickup));
+              console.log("pickupByDistance: " + JSON.stringify(pickup_by_distance));
+      
+              var destination = new google.maps.LatLng(
+                pickup.latitude,
+                pickup.longitude
+              );
+      
+              const getDistanceMatrix = (service, data) =>
+                new Promise((resolve, reject) => {
+                  service.getDistanceMatrix(data, (response, status) => {
+                    if (status === "OK") {
+                      console.log("Distance: " + JSON.stringify(response));
+                      var results = response.rows[0].elements;
+      
+                      pickup_by_distance.push({
+                        ...pickup,
+                        results,
+                      });
+                      console.log(
+                        "pickup after return: " + JSON.stringify(pickup_by_distance)
+                      );
+                      resolve(response);
+                    } else {
+                      reject(response);
+                    }
+                  });
+                });
+      
+              const result = await getDistanceMatrix(
+                service,
+                {
+                  origins: [origin],
+                  destinations: [destination],
+                  travelMode: "DRIVING",
+                  avoidHighways: false,
+                  avoidTolls: false,
+                },
+                function callback(response, status) {
+                  console.log("Distance: " + JSON.stringify(response));
+                  var results = response.rows[0].elements;
+      
+                  pickup_by_distance.push({
+                    ...pickup,
+                    results,
+                  });
+                  console.log(
+                    "pickup after return: " + JSON.stringify(pickup_by_distance)
+                  );
+                }
+              );
+            }
+      
+            console.log("Final Array: " + JSON.stringify(pickup_by_distance));
+      
             this.setState({
-                results: PICKUP_LOCATIONS
-            })
-        }
+              results: pickup_by_distance,
+            });
+          };
     }
 
     render() {
